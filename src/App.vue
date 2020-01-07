14ffceef8a3226ef4ddabfb900195c891a3cbac1
fengxinlu <15510578636@163.com>
2020-01-07 15:19:24 +0800

    测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>征婚启事哈哈</h1>  
+    <h1>征婚启事哈哈1</h1>  
     <img alt="Vue logo" src="./assets/01.jpeg" style="width: 600px; height: 801px">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
