ff49fede577470621b078762ef1ec80e2a9442f5
fengxinlu <fengxinlu@zhisland.com>
2020-01-07 10:44:19 +0800

    blog init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>征婚启事</h1>  
+    <img alt="Vue logo" src="./assets/01.jpeg" style="width: 600px; height: 800px">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+//   components: {
+//     HelloWorld
+//   }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
