f6df8cd7ba4a5c2d9c0f7f1ea65fdd1ae760e58b
fengxinlu <15510578636@163.com>
2020-01-08 10:57:08 +0800

    测试部署

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <h1>征婚启事1212335555</h1>  
-    <img alt="Vue logo" src="./assets/01.jpeg" style="width: 600px; height: 801px">
+    <h1>征婚启事</h1>  
+    <img alt="Vue logo" src="./assets/01.jpeg" style="width: 600px; height: 800px">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
